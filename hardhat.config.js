004231d74b31480520d39962e8ca5a4c59edb129
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-12-03 22:03:56 +0530

    updating config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
+
+const { PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+networks: {
+  arbitrumSepolia: {
+    url: "https://sepolia-rollup.arbitrum.io/rpc",
+    accounts: [process.env.PRIVATE_KEY]
+  }
+}
 };
